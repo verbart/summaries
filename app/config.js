ed31b6cfbe6e5f1467b473ed4d8188808f1b36db
Artem Verbat <artem.verbat@gmail.com>
2017-11-18 03:08:11 +0200

    Added the ability to read jwt secret from env variable

--- app/config.js
+++ app/config.js
@@ -12,10 +12,10 @@ if (!envs[env]) {
 
 const PORT = process.env.PORT || config.get('port');
 const MONGO_URI = process.env.MONGO_URI || config.get('mongo.uri');
-const JWT_SECRET = config.get('jwt.secret');
+const JWT_SECRET = process.env.JWT_SECRET || config.get('jwt.secret');
 
 if (!JWT_SECRET) {
-    throw Error('You must pass jwt secret string');
+  throw Error('You must pass jwt secret string');
 }
 
 
